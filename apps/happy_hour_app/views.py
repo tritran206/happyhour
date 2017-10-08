e249e961de83517396c90218a3994507dda9d3eb
Tri Tran <tritran20@gmail.com>
2017-10-07 21:06:31 -0700

    Added file structures for css, html, etc

--- apps/happy_hour_app/views.py
+++ apps/happy_hour_app/views.py
@@ -1,8 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+import googlemaps
 
 def index(request):
-    context = {
-    'name': 'Tri Tran'
-    }
-    return render(request, 'happy_hour_app/index.html', context)
+
+    return render(request, 'happy_hour_app/search_form.html')
+
+def results(request):
+    key = "AIzaSyBvbKFWJu80KQ1yxgxfP4k9hMVEaVBYamc"
+    location = request.POST
+    gmaps = GoogleMaps(key)
+
+    directions = gmaps.directions("18817 1st Ave W. Bothell, WA 98012", "20814 31st Ave W. Lynnwood, WA 98036")
+    print(directions['Distance'])
+    return redirect('/')
+
 # Create your views here.

8878d80032df8965b6d6cb45b11b39ffa4715daa
Tri Tran <tritran20@gmail.com>
2017-10-11 19:23:22 -0700

    Added database files, and locator api

--- apps/happy_hour_app/models.py
+++ apps/happy_hour_app/models.py
@@ -1,4 +1,25 @@
 from django.db import models
 
-
 # Create your models here.
+class Restaurant(models.Model):
+    name = models.CharField(max_length = 255, default='SOME STRING')
+    address = models.CharField(max_length = 255)
+    lat = models.FloatField()
+    lng = models.FloatField()
+    happy_hour_start = models.TimeField()
+    happy_hour_end = models.TimeField()
+    rating = models.DecimalField(max_digits = 5, decimal_places = 1)
+
+class Drink(models.Model):
+    restaurant_id = models.ForeignKey(Restaurant, related_name='restaurant')
+    name = models.CharField(max_length = 255)
+    price = models.DecimalField(max_digits = 5, decimal_places = 2)
+
+# class User(models.Model):
+#     user_name = models.CharField(max_length = 255)
+#     email = models.CharField(max_length = 255)
+#     password = models.CharField(max_length = 255)
+#
+# class Favorites(models.Model):
+#     user = models.ForeignKey(User, related_name='user')
+#     fav_restaurants = models.ForeignKey(Restaurant, related_name='restaurant')
--- apps/happy_hour_app/views.py
+++ apps/happy_hour_app/views.py
@@ -1,17 +1,30 @@
 from django.shortcuts import render, redirect
-import googlemaps
+from geopy.geocoders import Nominatim
+from geopy.distance import vincenty
 
 def index(request):
 
     return render(request, 'happy_hour_app/search_form.html')
 
 def results(request):
-    key = "AIzaSyBvbKFWJu80KQ1yxgxfP4k9hMVEaVBYamc"
-    location = request.POST
-    gmaps = GoogleMaps(key)
+    # key = "AIzaSyBvbKFWJu80KQ1yxgxfP4k9hMVEaVBYamc"
+    # location = request.POST
+    # gmaps = GoogleMaps(key)
+    #
+    # directions = gmaps.directions("18817 1st Ave W. Bothell, WA 98012", "20814 31st Ave W. Lynnwood, WA 98036")
+    # print(directions['Distance'])
 
-    directions = gmaps.directions("18817 1st Ave W. Bothell, WA 98012", "20814 31st Ave W. Lynnwood, WA 98036")
-    print(directions['Distance'])
+    geolocator = Nominatim()
+    location_a = geolocator.geocode("18817 1st Ave W. Bothell, WA 98012")
+    location_b = geolocator.geocode("20814 31st Ave W. Lynnwood, WA 98036")
+    lat_long_a = (location_a.latitude, location_a.longitude)
+    lat_long_b = (location_b.latitude, location_b.longitude)
+
+    print("location A: " + location_a.address)
+
+    print("location B: " + location_b.address)
+    print("distance from A and B is: " + str(vincenty(lat_long_a, lat_long_b).miles))
+
     return redirect('/')
 
 # Create your views here.

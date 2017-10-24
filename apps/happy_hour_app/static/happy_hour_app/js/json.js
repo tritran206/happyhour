b99d4ad934ba00b75dc7d1ae031be474ea2c305e
Eden Aygoda <aygodae@gmail.com>
2017-10-23 20:37:22 -0700

    Added json.js and json.html

---
+++ apps/happy_hour_app/static/happy_hour_app/js/json.js
@@ -0,0 +1,35 @@
+{
+	"description": "Restaurants in Seattle Area",
+	"source": "/search?address=1546+15th+Ave,+Seattle,+WA+98122&distance=5",
+	"Menu":[
+
+		{
+			"restaurant_id" : 1,
+			"restaurant_name" : "Bar Cotto",
+			"drink_name" : "Margarita",
+			"price" : 3,
+			"distance" : 1.2
+		},
+		{
+			"restaurant_id" : 1,
+			"restaurant_name" : "Bar Cotto",
+		  	"drink_name" : "Black Russian",
+			"price" : 5,
+			"distance" : 1.2
+ 		},
+ 		{
+		 	"restaurant_id" : 1,
+			"restaurant_name" : "Bar Cotto",
+		 	"drink_name" : "Negroni",
+			"price" : 6,
+			"distance" : 1.2
+ 		},
+ 		{
+		 	"restaurant_id" : 1,
+			"restaurant_name" : "Bar Cotto",
+		 	"drink_name" : "Martini",
+			"price" : 5,
+			"distance" : 1.2
+		}
+	]
+}
